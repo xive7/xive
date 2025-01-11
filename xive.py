ea657bfc887a8ac54e9815f56ecec2783745ffa1
xive7 <mdahnaf094@gmail.com>
2025-01-11 22:30:43 +0600

    Create xive.py

---
+++ xive.py
@@ -0,0 +1,10 @@
+#!/data/data/com.termux/files/usr/bin/python
+import platform,os
+#####
+os.system("git pull")
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import c
+elif bit == '32bit':
+    print('\033[1;97m[✓] SORRY BRO YOUR DEVIS IS 32 BIT ')
+ 
