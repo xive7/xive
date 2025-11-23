7f93d10fd5458acedf25d137536994ad2e5230d5
xive7 <mdahnaf094@gmail.com>
2025-11-23 21:46:04 +0600

    Update XIVE.py

--- XIVE.py
+++ XIVE.py
@@ -8,14 +8,14 @@ if fuck == '64bit':
     os.system('clear')
     print('[•] YOUR DEVICE IS 64 BIT')
     time.sleep(2)
-    from c.cpython-312 import c.cpython-312
+    from c import c
     cc.cpython-312()
 if fuck == '32bit':
     os.system('git pull')
     os.system('clear')
     print('[•] YOUR DEVICE IS 32 BIT')
     time.sleep(2)
-    from c.cpython-312 import c.cpython-312
-    c.cpython-312() 
+    from c import c
+    c.cpython-312()
 os.system('clear')
 print('\x1b[1;97m Soon Your Device Supported Tools ')
