734a1bb1800d8ab81a8be81499767d91a345cea9
xive7 <mdahnaf094@gmail.com>
2025-11-23 21:07:27 +0600

    Create XIVE.py

---
+++ XIVE.py
@@ -0,0 +1,22 @@
+import os
+import platform
+import time
+import c
+os.system("pip install requests")
+fuck = platform.architecture()[0]
+if fuck == '64bit':
+    os.system('git pull')
+    os.system('clear')
+    print('[•] YOUR DEVICE IS 64 BIT')
+    time.sleep(2)
+    from c import c
+    c()
+if fuck == '32bit':
+    os.system('git pull')
+    os.system('clear')
+    print('[•] YOUR DEVICE IS 32 BIT')
+    time.sleep(2)
+    from c import c
+    c() 
+os.system('clear')
+print('\x1b[1;97m Soon Your Device Supported Tools ')
